b849f4f8e6003e357669f941c144593a521927c8
desmondchiam18 <desmondchiam18@gmail.com>
2021-12-20 12:45:48 +0000

    done exercise 3

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    let name = ""
+    let gender = ""
+
+    async function getName() {
+    fetch('https://api.genderize.io/?name=' + name)
+      .then((response) => response.json())
+      .then((data) => {
+        gender = data.gender; 
+      });
+    }
+</script>
+
+
+<h1>Guess gender</h1>
+
+<form on:submit|preventDefault={getName}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Gender: {gender}</p>
+
